25c5727c140b67e5babd09e59eba45224d5a9e52
nferdica <ncaferme@gmail.com>
2024-01-12 00:23:50 -0300

    Commited upgrade

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Nathan Ferdinando'
+    })
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('SOBRE')
