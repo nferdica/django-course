a94b74245d94e87e652682c3867e76ddf0579a71
nferdica <ncaferme@gmail.com>
2024-01-12 00:39:24 -0300

    Remove faults

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Nathan Ferdinando'
     })
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
